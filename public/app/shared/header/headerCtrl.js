2dd8f0bb24bfc85b386a9811225eeb954430f64d
Gene Nakagaki <gene.nakagaki@gmail.com>
2015-11-07 01:42:55 -0500

    added external link

--- public/app/shared/header/headerCtrl.js
+++ public/app/shared/header/headerCtrl.js
@@ -4,20 +4,36 @@ angular.module('headerModule')
 .controller('HeaderCtrl', ['$scope', function($scope) {
   $scope.template = '/app/shared/header/headerView.html';
 
-  function Link(name, url) {
+  // Navigation Link
+  function NavLink(name, url) {
     this.name = name;
     this.url  = url;
   }
 
-  var homeLink      = new Link("Home",     "/");
-  var projectsLink  = new Link("Projects", "/projects");
-  var aboutLink     = new Link("About",    "/about");
-  var contactLink   = new Link("Contact",  "#main-footer")
+  var homeLink      = new NavLink('Home',     '/');
+  var projectsLink  = new NavLink('Projects', '/projects');
+  var aboutLink     = new NavLink('About',    '/about');
+  var contactLink   = new NavLink('Contact',  '#main-footer');
 
-  $scope.linkList = [
+  $scope.navLinkList = [
     homeLink,
     projectsLink,
     aboutLink,
     contactLink
+  ];
+
+  // External Link
+  function ExtLink(name, url, img) {
+    this.name = name;
+    this.url  = url;
+    this.img  = img;
+  }
+
+  var githubLink   = new ExtLink('GitHub',   'https://github.com/kagen47', '');
+  var linkedinLink = new ExtLink('Linkedin', '#', '');
+
+  $scope.extLinkList = [
+    githubLink,
+    linkedinLink
   ];
 }]);
